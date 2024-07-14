24121312e9b0fd03a0270e47c39b766af5aa4799
m-sarabi <m.sarabi.jkd@gmail.com>
2024-07-14 12:41:11 +0330

    Added vertical gridlines and other improvements

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-class candle {
+const WIDTH_SCALE = 0.75;
+
+class Candle {
     constructor(index, open, high, low, close, range, x, y) {
         this.index = index;
         this.open = open;
@@ -8,6 +10,7 @@ class candle {
         this.range = range;
         this.x = x;
         this.y = y;
+        this.greenCandle = open > close;
 
         this.scale = this.range.containerHeight / (this.range.highestHigh - this.range.lowestLow);
 
@@ -26,7 +29,7 @@ class candle {
 
         // temp properties
         let rawWidth = this.range.containerWidth / this.range.length;
-        let width = rawWidth * 0.75;
+        let width = rawWidth * WIDTH_SCALE;
         let x = this.index * rawWidth + (rawWidth - width) / 2;
         let y = (this.range.highestHigh - this.high) * this.scale;
 
@@ -43,7 +46,7 @@ class candle {
         this.bodyElement.setAttributeNS(null, 'height', sizes.bodySize.toString());
 
         // add class to the body
-        if (this.open < this.close) {
+        if (this.greenCandle) {
             this.bodyElement.classList.add('bull');
         } else {
             this.bodyElement.classList.add('bear');
@@ -58,6 +61,7 @@ class candle {
         this.upperShadowElement.setAttributeNS(null, 'x2', (x + width / 2).toString());
         this.upperShadowElement.setAttributeNS(null, 'y2', (y + sizes.upperShadowSize).toString());
         this.upperShadowElement.setAttributeNS(null, 'stroke', 'black');
+        this.upperShadowElement.setAttributeNS(null, 'stroke-width', '2');
 
 
         // lower shadow of the candle as a line svg element
@@ -68,6 +72,17 @@ class candle {
         this.lowerShadowElement.setAttributeNS(null, 'x2', (x + width / 2).toString());
         this.lowerShadowElement.setAttributeNS(null, 'y2', (y + sizes.bodySize + sizes.lowerShadowSize + sizes.upperShadowSize).toString());
         this.lowerShadowElement.setAttributeNS(null, 'stroke', 'black');
+        this.lowerShadowElement.setAttributeNS(null, 'stroke-width', '2');
+
+        // add class to shadows
+        for (let shadow of [this.upperShadowElement, this.lowerShadowElement]) {
+            if (this.greenCandle) {
+                shadow.classList.add('bull');
+            } else {
+                shadow.classList.add('bear');
+            }
+            shadow.classList.add('shadow');
+        }
 
         // add event listener
         this.candleGroup.addEventListener('click', this.onClick.bind(this));
@@ -87,6 +102,71 @@ class candle {
     }
 }
 
+class VerticalLines {
+    constructor(range, minSpacing = 20) {
+        this.range = range;
+        this.minSpacing = minSpacing;
+
+        this.spacing = 1;
+        this.verticalGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    }
+
+    lines() {
+        let rawWidth = this.range.containerWidth / this.range.length;
+        while (rawWidth * this.spacing < this.minSpacing) {
+            this.spacing++;
+        }
+        let x = rawWidth / 2;
+        let y = this.range.containerHeight;
+        while (x < this.range.containerWidth) {
+            let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            line.setAttributeNS(null, 'x1', x.toString());
+            line.setAttributeNS(null, 'y1', '0');
+            line.setAttributeNS(null, 'x2', x.toString());
+            line.setAttributeNS(null, 'y2', y.toString());
+            line.setAttributeNS(null, 'stroke', 'black');
+            line.setAttributeNS(null, 'opacity', '0.3');
+            this.verticalGroup.appendChild(line);
+            x += rawWidth * this.spacing;
+        }
+
+        return this.verticalGroup;
+    }
+}
+
+function drawCandles(container, ohlcData) {
+    let lowestLow = Math.min(...ohlcData.low);
+    let highestHigh = Math.max(...ohlcData.high);
+
+    let range = {
+        highestHigh: highestHigh,
+        lowestLow: lowestLow,
+        length: ohlcData.date.length,
+    };
+    let containerStyle = window.getComputedStyle(container);
+    range['containerHeight'] = parseInt(containerStyle.height);
+    range['containerWidth'] = parseInt(containerStyle.width);
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    // set the width and height of the svg element
+    svg.setAttributeNS(null, 'width', '100%');
+    svg.setAttributeNS(null, 'height', '100%');
+
+    // drawing the vertical grid lines
+    let verticalLines = new VerticalLines(range);
+    svg.appendChild(verticalLines.lines());
+
+    for (let i = 0; i < ohlcData.date.length; i++) {
+        let candle = new Candle(i, ohlcData.open[i], ohlcData.high[i], ohlcData.low[i], ohlcData.close[i], range);
+        svg.appendChild(candle.body());
+    }
+
+    container.appendChild(svg);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const ohlcData = {
         'date': [1, 2, 3, 4, 5, 6],
@@ -96,34 +176,11 @@ document.addEventListener('DOMContentLoaded', function () {
         'close': [1.6, 4, 6, 4.9, 3.5, 4.2]
     };
 
-    let lowestLow = Math.min(...ohlcData.low);
-    let highestHigh = Math.max(...ohlcData.high);
+    let container = document.getElementById('chart');
 
-    let range = {
-        highestHigh: highestHigh,
-        lowestLow: lowestLow,
-        length: ohlcData.date.length,
-    };
+    drawCandles(container, ohlcData);
 
-    let container = document.getElementById('test');
-    let containerStyle = window.getComputedStyle(container);
-
-    range['containerHeight'] = parseInt(containerStyle.height);
-    range['containerWidth'] = parseInt(containerStyle.width);
-    console.log(range.containerHeight, range.containerWidth);
-
-    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-
-    // set the width and height of the svg element
-    svg.setAttributeNS(null, 'width', '100%');
-    svg.setAttributeNS(null, 'height', '100%');
-
-    // draw the first candle
-    for (let i = 0; i < ohlcData.date.length; i++) {
-        let candle1 = new candle(i, ohlcData.open[i], ohlcData.high[i], ohlcData.low[i], ohlcData.close[i], range);
-        svg.appendChild(candle1.body());
-    }
-    // let candle1 = new candle(ohlcData.date[0], ohlcData.open[0], ohlcData.high[0], ohlcData.low[0], ohlcData.close[0], range);
-    // svg.appendChild(candle1.body());
-    container.appendChild(svg);
+    window.addEventListener('resize', function () {
+        drawCandles(container, ohlcData);
+    });
 });
